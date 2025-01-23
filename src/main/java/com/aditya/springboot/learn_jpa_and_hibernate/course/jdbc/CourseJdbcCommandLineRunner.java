4b5eab518a379f37d7ff83d16a91526348e74211
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-23 12:20:10 +0530

    insertion using spring jdbc template not hard coded

--- src/main/java/com/aditya/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/aditya/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.aditya.springboot.learn_jpa_and_hibernate.course.jdbc;
 
+import com.aditya.springboot.learn_jpa_and_hibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,13 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     @Autowired
     private CourseJdbcRepository repository;
     @Override
+
+
     public void run(String... args) throws Exception {
-        repository.insert();
+        int id = 1;
+        repository.insert(new Course(id, "learn Azure", "in28minutes"));
+        repository.insert(new Course(++id, "learn Devops", "in28minutes"));
+        repository.insert(new Course(++id, "learn SpringBoot", "in28minutes"));
+
     }
 }
--- src/main/java/com/aditya/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/aditya/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,5 +1,5 @@
 package com.aditya.springboot.learn_jpa_and_hibernate.course.jdbc;
-
+import com.aditya.springboot.learn_jpa_and_hibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,10 +12,11 @@ public class CourseJdbcRepository {
           //THIS THREE DOUBLE QUOTES IS CALLED A TEXT BLOCK AND IN HERE WE CAN TYPE WHATEVER QUERY WE WANT
           """ 
           insert into course (id, name, author) 
-          values(1, 'Learn AWS', 'in28minutes');
+          values(?, ?, ?);
           
           """;
-  public void insert(){
-      springJdbcTemplate.update(INSERT_QUERY);
-  }
+    public void insert(Course course) {
+        springJdbcTemplate.update(INSERT_QUERY,
+                course.getId(), course.getName(), course.getAuthor());
+    }
 }
