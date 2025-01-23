3e5f834f3cdc6349e917f687bc41d3b693d96f5d
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-23 16:25:54 +0530

    spring data jpa (advantage --> need to write even less amout of code)

--- src/main/java/com/aditya/springboot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/aditya/springboot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.aditya.springboot.learn_jpa_and_hibernate.course;
 
 import com.aditya.springboot.learn_jpa_and_hibernate.course.jdbc.CourseJdbcRepository;
 import com.aditya.springboot.learn_jpa_and_hibernate.course.jpa.CourseJpaRepository;
+import com.aditya.springboot.learn_jpa_and_hibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,17 +12,20 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 //    @Autowired
 //    private CourseJdbcRepository repository;
 
+//    @Autowired
+//    private CourseJpaRepository repository;
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
     @Override
     public void run(String... args) throws Exception {
         int id = 1;
-        repository.insert(new Course(id, "learn Azure jpa", "in28minutes"));
-        repository.insert(new Course(++id, "learn Devops jpa", "in28minutes"));
-        repository.insert(new Course(++id, "learn SpringBoot jpa", "in28minutes"));
-        repository.deleteById(1);
-        System.out.println(repository.findById(2));
-        System.out.println(repository.findById(3));
+        repository.save(new Course(id, "learn Azure jpa", "in28minutes"));
+        repository.save(new Course(++id, "learn Devops jpa", "in28minutes"));
+        repository.save(new Course(++id, "learn SpringBoot jpa", "in28minutes"));
+        repository.deleteById(1L);
+        System.out.println(repository.findById(2L));
+        System.out.println(repository.findById(3L));
+
 
     }
 }
